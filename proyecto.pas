7a5b9a1fab3e1f6a36873d082c27370432017be9
Samuel <samuelpl0888@gmail.com>
2023-06-18 17:23:19 -0400

    Agregar menus

--- proyecto.pas
+++ proyecto.pas
@@ -1,9 +1,8 @@
-
 Program proyectoProgram;
 
 Uses crt;
 
-Const 
+Const
   LIMITE = 30;
   CELDA = '#';
   PARED = '|';
@@ -22,14 +21,22 @@ Const
   Z = 90;
   X = 88;
 
-
-Type 
+Type
   vector = array[1..LIMITE] Of integer;
   mapa = array[1..LIMITE, 1..LIMITE] Of char;
-	
-Var
-  fil, col: Integer;
+  mapa2 = array[1..LIMITE, 1..LIMITE] Of Integer;    // MAPA SAMUEL
 
+// Variables principales
+Var
+  fil, col: integer; // Juego
+  terreno: mapa;
+	terreno2: mapa2;   // MAPA SAMUEL
+  ch: char;
+	nave: vector;
+	opc: Integer; // Menu
+	salir, volver: Boolean;
+
+// Función que valida tanto filas como columnas
 Function validarDim(n: Integer; mensaje: String):Integer;
 Begin
    Repeat
@@ -40,23 +47,122 @@ Begin
 	 Until (n>=1) and (n<=LIMITE);
 
 	 validarDim:= n;
-End;	 
+End;
 
-Procedure relleno(Var terreno: mapa; nave: vector; fila, colum: integer);
+procedure generarMapa(var terreno: mapa2; fil,col:integer);
+var
+  i,j:integer;
+begin
+  randomize;
+  for i:=1 to fil do
+     begin
+       for j:=1 to col do
+         begin
+            terreno[i,j]:=random(9)+1;
+         end;
+     end;
+end;
 
-Var 
+Procedure imprimirMapa(terreno: mapa2; fil, col:Integer);
+Var
+   i, j: Integer;
+Begin
+
+   for i:=1 to fil Do
+	 Begin
+	    for j:=1 to col Do
+			Begin
+			   write(terreno[i,j], ' ');
+			End;
+			writeLn;
+	 End;
+End;
+			   
+
+// Menu tutorial
+Procedure menuTutorial;
+Begin
+
+  salir := false;
+  volver := false;
+  Repeat
+	  clrscr;
+	  Delay(300);
+		writeLn('---Bienvenido al tutorial de LE NAVE---');
+		writeLn('Selecciona una opcion: ');
+		writeLn('1. Controles');
+		writeLn('2. Como funciona?');
+		writeLn('3. Truquitos');
+		writeLn('4. Volver');
+		writeLn('5. Salir');
+		Readln(opc);
+		case opc of
+		  1: writeLn('Los controles son...');
+			2: writeLn('Como tu quieras');
+			3: writeLn('No hay truquitos');
+			4: volver:= True;
+			5: salir:= True;
+			Else
+			Begin
+			  writeLn('La opcion ', opc, ' no existe');
+				readLn;
+			End;
+		End;
+	Until (salir) or (volver);
+End;
+// Menu opcion jugar
+Procedure menuJugar;
+Begin
+
+  salir := false;
+  volver := false;
+  Repeat
+	  clrscr;
+		Delay(300);
+		writeLn('---LE NAVE---');
+		writeLn('Selecciona una de las siguientes modalidades de juego: ');
+		writeln('1. Mapa personalizado');
+    writeln('2. Mapa al azar');
+    writeln('3. Volver');
+    writeln('4. Salir');
+    Readln(opc);
+    case opc of
+		  1: Begin
+			      Clrscr;
+						Delay(300);
+				    fil:= validarDim(fil, 'filas');
+						col:= validarDim(col, 'columnas');
+						generarMapa(terreno2, fil,col);
+						imprimirMapa(terreno2,fil,col);
+						Readln;
+			   End;
+      2: writeln('Mapa al azar');
+      3: volver := true;
+      4: salir := true;
+		  Else
+			Begin
+			   writeLn('Error, la opcion', opc, ' no existe');
+				 readLn;
+			End;
+	  End;
+	Until(salir) or (volver);
+End;
+
+Procedure relleno(Var terreno: mapa; nave: vector; fil, col: integer);
+
+Var
   i, j: integer;
 Begin
 
   randomize;
 
-  nave[1] := random(fila)+1;
-  nave[2] := random(colum)+1;
+  nave[1] := random(fil)+1;
+  nave[2] := random(col)+1;
 
-  For i := 1 To fila Do
+  For i := 1 To fil Do
     Begin
       j := 1;
-      For j:= 1 To colum Do
+      For j:= 1 To col Do
         Begin
           If ((i = nave[1]) And (j = nave[2])) Then
             Begin
@@ -70,9 +176,9 @@ End;
 
 // Funciones
 
-Procedure Personaje(Var nave: vector; fila, colum, tecla: integer);
+Procedure Personaje(Var nave: vector; fil, col, tecla: integer);
 
-Var 
+Var
   i: integer;
 
 Begin
@@ -82,13 +188,13 @@ Begin
   If ((tecla = W) And (nave[1] > 1)) Then
     nave[1] := nave[1] - 1;
 
-  If ((tecla = S) And (nave[1] < fila)) Then
+  If ((tecla = S) And (nave[1] < fil)) Then
     nave[1] := nave[1] + 1;
 
   If ((tecla = A) And (nave[2] > 1)) Then
     nave[2] := nave[2] - 1;
 
-  If ((tecla = D) And (nave[2] < colum)) Then
+  If ((tecla = D) And (nave[2] < col)) Then
     nave[2] := nave[2] + 1;
 
 
@@ -100,19 +206,19 @@ Begin
       nave[2] := nave[2] - 1;
     End;
 
-  If ((tecla = E) And (nave[1] > 1) And (nave[2] < colum)) Then
+  If ((tecla = E) And (nave[1] > 1) And (nave[2] < col)) Then
     Begin
       nave[1] := nave[1] - 1;
       nave[2] := nave[2] + 1;
     End;
 
-  If ((tecla = Z) And (nave[1] < fila) And (nave[2] > 1)) Then
+  If ((tecla = Z) And (nave[1] < fil) And (nave[2] > 1)) Then
     Begin
       nave[1] := nave[1] + 1;
       nave[2] := nave[2] - 1;
     End;
 
-  If ((tecla = X) And (nave[1] < fila) And (nave[2] < colum)) Then
+  If ((tecla = X) And (nave[1] < fil) And (nave[2] < col)) Then
     Begin
       nave[1] := nave[1] + 1;
       nave[2] := nave[2] + 1;
@@ -123,9 +229,9 @@ End;
 
 // LEER EL MAPA FINAL PROCEDIMIENTO
 
-Procedure leerMapa(terreno: mapa; fila, colum: integer);
+Procedure leerMapa(terreno: mapa; fil, col: integer);
 
-Var 
+Var
   i, j: integer;
 Begin
   clrscr;
@@ -135,10 +241,10 @@ Begin
 
   // Colocar Celdas:
 
-  For i := 1 To fila Do
+  For i := 1 To fil Do
     Begin
       j := 0;
-      For j := 1 To colum Do
+      For j := 1 To col Do
         Begin
           If ((j = 1) And (i < 10)) Then
             write(i, '  ', PARED, ' ');
@@ -165,34 +271,49 @@ Begin
   writeln('Presiona ENTER para salir');
 End;
 
-// Muestra
-
-Var 
-  fila, colum: integer;
-  terreno: mapa;
-  ch: char;
-  nave: vector;
-
+// PROGRAMA PRINCIPAL ------------------------------
 Begin
   clrscr;
+	salir:= false; // Inicializando estados de salir y volver
+	volver:= false;
 
-	// Pedir filas y columnas al usuario
+	{// Pedir filas y columnas al usuario
 	fil:= validarDim(fil, 'filas');
 	col:= validarDim(col, 'columnas');
 
-  n := 10;
-
-  fila := 8;
-  colum := 9;
+  fil := 8;
+  col := 9;
   nave[1] := 1;
   nave[2] := 2;
 
 
-  relleno(terreno, nave, fila, colum);
-  Personaje(nave, fila, colum, 86);
+  relleno(terreno, nave, fil, col);
+  Personaje(nave, fil, col, 86); }
+
+ { Repeat
+    leerMapa(terreno, fil, col);
+    ch := readkey;
+  Until (ord(ch) = ENTER);}
+
+	// MENU PRINCIPAL
 
   Repeat
-    leerMapa(terreno, fila, colum);
-    ch := readkey;
-  Until (ord(ch) = ENTER);
+	   clrscr;
+     writeln('---Bienvenido a L nave---');
+     writeln('1. Jugar');
+     writeln('2. Tutorial');
+     writeln('3. Salir');
+     readln(opc);
+		 writeLn;
+		 case opc of
+		    1: menuJugar;
+		    2: menuTutorial;
+		    3: salir:= true;
+				Else
+				Begin
+				   writeLn('Error, la opcion ', opc, ' no existe');
+					 Readln;
+				End;
+		 End;
+  Until(salir);
 End.
