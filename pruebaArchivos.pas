80d5920ca0520a50a87452b5216847949915d96c
Samuel Palacios <samuelpl0888@gmail.com>
2023-06-20 18:05:27 -0400

    Guardando datos del archivo en objeto

--- pruebaArchivos.pas
+++ pruebaArchivos.pas
@@ -5,17 +5,25 @@ Const
 
 Type
   matriz = Array[1..limite, 1..limite] of Integer;
-  datosMapa = record
+  datamapa = record
     dimensiones: record
       fil: Integer;
       col: Integer;
     end;
+    nave: record
+      posicionX: Integer;
+      posicionY: Integer;
+    end;
+    planetaT: record
+      posicionX: Integer;
+      posicionY: Integer;
+    end;
   end;
-
 
 Var
   m: matriz; fil,col:Integer;
 	archivo: text;
+	datosMapa: dataMapa;
 
 Procedure leerArchivo(var archivo: text);
 Var
@@ -23,7 +31,8 @@ Var
 Begin
 
   reset(archivo);
-	Read(archivo, fil, col);
+	// Guardar fila y columna en el objeto
+	Read(archivo, datosMapa.dimensiones.fil, datosMapa.dimensiones.col);
 
 End;
 
@@ -32,7 +41,7 @@ Begin
 
   Assign(archivo, 'E:\Default Folders\Samuel\Desktop\UCAB 2do semestre\project-ucab\est.dat');
 	leerArchivo(archivo);
-	writeLn('El valor de filas es: ', fil, ' el valor de columnas es: ', col);
+	writeLn('El valor de filas es: ', datosMapa.dimensiones.fil, ' el valor de columnas es: ', datosMapa.dimensiones.col);
 
 Readln;
 End.
