0be6948c41109297f7724b4c7654c271ad2de3f8
Samuel Palacios <samuelpl0888@gmail.com>
2023-06-20 21:13:43 -0400

    Update archivos

--- pruebaArchivos.pas
+++ pruebaArchivos.pas
@@ -33,11 +33,12 @@ Var
 	datosMapa: dataMapa;
 	estrellas: Integer;
 	estrellasMod: Integer;
+	cantidadEstrellas: Integer;
 
-Procedure leerArchivo(var archivo: text);
+Procedure leerArchivo(var archivo: text; var cantidadEstrellas:Integer);
 Var
   i, j: Integer;
-	cantEstrellas,cantEstrellas_1, cantEstrellas_2: Integer;
+	cantEstrellas_1, cantEstrellas_2: Integer;
 Begin
 
   reset(archivo);
@@ -47,24 +48,33 @@ Begin
 	Read(archivo, datosMapa.nave.posicionX, datosMapa.nave.posicionY);
 	// Guardar posicion planetaT (X,Y)
 	Read(archivo, datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY);
+
+	// ---- LEER CANTIDAD DE ESTRELLAS (ver si esto se puede pasar a un procedimiento aparte)
 	// Leer el primer numero de la cantidad de estrellas del archivo y comprobar
 	Read(archivo, cantEstrellas_1);
 	// Nro de estrellas < a 10 (0 n) (ej 0 7) = 7 estrellas
 	if (cantEstrellas_1 = 0) Then
-	   Read(archivo, datosMapa.estrellas.cantidad) // Guardar cantidad de estrellas en el objeto
+  Begin
+	  Read(archivo, datosMapa.estrellas.cantidad); // Guardar cantidad de estrellas en el objeto
+		cantidadEstrellas:= datosMapa.estrellas.cantidad;
+	End
 	Else
-	// Nro de estrellas > a 10 (1 n ó 2 n)
-	// Agarra el primer numero de la linea y lo une con el segundo (ej 1 5) = 15 estrellas
+	// Nro de estrellas > a 10 (1 n ó 2 n) Agarra el primer nro de la linea y lo une con el sig (ej 1 5) = 15 estrellas
 	Begin
-	   Read(archivo, cantEstrellas_2);
-		 writeLn('Estrellas 2: ', cantEstrellas_2);
-		 cantEstrellas:= cantEstrellas_1 * 10 + cantEstrellas_2;
-		 datosMapa.estrellas.cantidad:= cantEstrellas;
+	  Read(archivo, cantEstrellas_2);
+		writeLn('Estrellas 2: ', cantEstrellas_2);
+		cantidadEstrellas:= cantEstrellas_1 * 10 + cantEstrellas_2;
+		datosMapa.estrellas.cantidad:= cantidadEstrellas;
+	End;
+
+	// ---- Leer coordenadas de estrellas
+	for i:=1 to cantidadEstrellas Do
+	Begin
+
 	End;
 	   	   
 
-	
-	
+
 
 End;
 
@@ -72,11 +82,11 @@ End;
 Begin
 
   Assign(archivo, 'E:\Default Folders\Samuel\Desktop\UCAB 2do semestre\project-ucab\est.dat');
-	leerArchivo(archivo);
+	leerArchivo(archivo, cantidadEstrellas);
 	writeLn('El valor de filas es ', datosMapa.dimensiones.fil, ' y de columnas ', datosMapa.dimensiones.col);
 	writeLn('Las coordenadas de la nave son: ', datosMapa.nave.posicionX, ' y ', datosMapa.nave.posicionY);
 	writeLn('Las coordenadas de el planeta T son: ', datosMapa.planetaT.posicionX, ' y ', datosMapa.planetaT.posicionY);
-	writeLn('La cantidad de estrellas es: ', datosMapa.estrellas.cantidad);
+	writeLn('La cantidad de estrellas es: ', cantidadEstrellas);
 	estrellas:= datosMapa.estrellas.cantidad;
 	estrellasMod:= estrellas + 2;
   writeLn(estrellasMod);
