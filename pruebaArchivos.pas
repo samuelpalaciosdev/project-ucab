8846693276d72582ca50e5bffef0a01bf4dd1f9f
Samuel Palacios <samuelpl0888@gmail.com>
2023-06-20 20:13:34 -0400

    Añadir funcion que verifica la cantidad de estrellas

--- pruebaArchivos.pas
+++ pruebaArchivos.pas
@@ -2,6 +2,7 @@ Program prueba_archivos;
 
 Const
   limite = 30;
+	// Debería ser una variable que cambia segun el max de estrellas expresado en el archivo
 	limiteEstrellas = 10;
 
 Type
@@ -24,14 +25,19 @@ Type
 			coordenadas: Array[1..limiteEstrellas] of coordenada;
 		end;
   end;
+	
+// Variables principales	
 Var
   m: matriz; fil,col:Integer;
 	archivo: text;
 	datosMapa: dataMapa;
+	estrellas: Integer;
+	estrellasMod: Integer;
 
 Procedure leerArchivo(var archivo: text);
 Var
   i, j: Integer;
+	cantEstrellas,cantEstrellas_1, cantEstrellas_2: Integer;
 Begin
 
   reset(archivo);
@@ -41,8 +47,24 @@ Begin
 	Read(archivo, datosMapa.nave.posicionX, datosMapa.nave.posicionY);
 	// Guardar posicion planetaT (X,Y)
 	Read(archivo, datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY);
-	// Guardar cantidad de estrellas
-	Read(archivo, datosMapa.estrellas.cantidad);
+	// Leer el primer numero de la cantidad de estrellas del archivo y comprobar
+	Read(archivo, cantEstrellas_1);
+	// Nro de estrellas < a 10 (0 n) (ej 0 7) = 7 estrellas
+	if (cantEstrellas_1 = 0) Then
+	   Read(archivo, datosMapa.estrellas.cantidad) // Guardar cantidad de estrellas en el objeto
+	Else
+	// Nro de estrellas > a 10 (1 n ó 2 n)
+	// Agarra el primer numero de la linea y lo une con el segundo (ej 1 5) = 15 estrellas
+	Begin
+	   Read(archivo, cantEstrellas_2);
+		 writeLn('Estrellas 2: ', cantEstrellas_2);
+		 cantEstrellas:= cantEstrellas_1 * 10 + cantEstrellas_2;
+		 datosMapa.estrellas.cantidad:= cantEstrellas;
+	End;
+	   	   
+
+	
+	
 
 End;
 
@@ -54,5 +76,11 @@ Begin
 	writeLn('El valor de filas es ', datosMapa.dimensiones.fil, ' y de columnas ', datosMapa.dimensiones.col);
 	writeLn('Las coordenadas de la nave son: ', datosMapa.nave.posicionX, ' y ', datosMapa.nave.posicionY);
 	writeLn('Las coordenadas de el planeta T son: ', datosMapa.planetaT.posicionX, ' y ', datosMapa.planetaT.posicionY);
+	writeLn('La cantidad de estrellas es: ', datosMapa.estrellas.cantidad);
+	estrellas:= datosMapa.estrellas.cantidad;
+	estrellasMod:= estrellas + 2;
+  writeLn(estrellasMod);
+
+
 Readln;
 End.
