7e97292b7634ac3dd704b8e7ba40462a071c4582
Daniel <rossdaniel5018@gmail.com>
2023-06-21 16:40:17 -0400

    ..

--- matrizConArchivo.pas
+++ matrizConArchivo.pas
@@ -1,153 +1,189 @@
+
 Program matriz_archivos;
 
-Const
+Const 
   limite = 30;
-	limiteEstrellas = 15;
-	limiteDestructores = 10;
+  limiteEstrellas = 15;
+  limiteDestructores = 10;
 
-Type
-  matriz = Array[1..limite, 1..limite] of Char;
-	// Tipo de dato usado en varias keys del objeto
-	coordenada = Record
-	  posicionX: Integer;
-		posicionY: Integer;
-	end;
-	// Objeto donde se almacena toda la info del archivo
-  datamapa = record
-    dimensiones: record
+Type 
+  matriz = Array[1..limite, 1..limite] Of Char;
+  // Tipo de dato usado en varias keys del objeto
+  coordenada = Record
+    posicionX: Integer;
+    posicionY: Integer;
+  End;
+  // Objeto donde se almacena toda la info del archivo
+  datamapa = Record
+    // datamapa.estrellas.coordenadas
+    dimensiones: Record
       fil: Integer;
       col: Integer;
-    end;
+    End;
     nave: coordenada;
     planetaT: coordenada;
-		estrellas: Record
-		  cantidad: Integer;
-			coordenadas: Array[1..limiteEstrellas] of coordenada;
-		end;
-		destructores: Record
-		  cantidad: Integer;
-			coordenadas: Array[1..limiteEstrellas] of coordenada;
-		end;
-  end;
+    estrellas: Record
+      cantidad: Integer;
+      coordenadas: Array[1..limiteEstrellas] Of coordenada;
+    End;
+    destructores: Record
+      cantidad: Integer;
+      coordenadas: Array[1..limiteEstrellas] Of coordenada;
+    End;
+  End;
 
   // Variables principales
-Var
-  m: matriz; fil,col:Integer;
-	archivo: text;
-	datosMapa: dataMapa;
+
+Var 
+  m: matriz;
+  fil,col: Integer;
+  archivo: text;
+  datosMapa: dataMapa;
+
+
+
 
 // Procedimiento reutilizable para leer info de las (ESTRELLAS Y DESTRUCTORES) del archivo
-procedure leerCantidadYCoordenadas(var archivo: Text; var cantidad: Integer; var coordenadas: array of coordenada);
-var
+Procedure leerCantidadYCoordenadas(Var archivo: Text; Var cantidad: Integer; Var
+                                   coordenadas: Array Of coordenada);
+
+Var 
   i, cant_1, cant_2: Integer;
-begin
-  // Leer el primer numero de la cantidad de (estrellas o destructores) del archivo y comprobar si es > 10 o < 10
-	// Nro < a 10 (0 n) (ej. 0 7) = 7
+Begin
+
+
+
+// Leer el primer numero de la cantidad de (estrellas o destructores) del archivo y comprobar si es > 10 o < 10
+  // Nro < a 10 (0 n) (ej. 0 7) = 7
   Read(archivo, cant_1);
-  if cant_1 = 0 then
-  Begin
-    Read(archivo, cantidad);
-  End
-	// Nro > a 10 (1 n ó 2 n) Agarra el primer nro de la linea y lo une con el sig (ej 1 5) = 15
+  If cant_1 = 0 Then
+    Begin
+      Read(archivo, cantidad);
+    End
+
+
+
+// Nro > a 10 (1 n ï¿½ 2 n) Agarra el primer nro de la linea y lo une con el sig (ej 1 5) = 15
   Else
-  Begin
-    Read(archivo, cant_2);
-    cantidad := cant_1 * 10 + cant_2;
-  End;
-	{ ---- Leer coordenadas de (estrellas o destructores), guarda la posicion de cada elemento como un
+    Begin
+      Read(archivo, cant_2);
+      cantidad := cant_1 * 10 + cant_2;
+    End;
+
+
+
+{ ---- Leer coordenadas de (estrellas o destructores), guarda la posicion de cada elemento como un
 	       obj de coordenadas dentro de un array}
-  for i := 1 to cantidad do
-  Begin
-    Read(archivo, coordenadas[i].posicionX, coordenadas[i].posicionY);
-  End;
-end;
+  For i := 1 To cantidad Do
+    Begin
+      Read(archivo, coordenadas[i].posicionX, coordenadas[i].posicionY);
+    End;
+End;
 
 
-Procedure leerArchivo(var archivo: text; var datosMapa: dataMapa);
-Var
+Procedure leerArchivo(Var archivo: text; Var datosMapa: dataMapa);
+
+Var 
   i, j: Integer;
 Begin
-	// Abrir archivo
+  // Abrir archivo
   reset(archivo);
-	// Guardar fila y columna en el objeto
-	Read(archivo, datosMapa.dimensiones.fil, datosMapa.dimensiones.col);
-	// Guardar posicion nave     (X,Y)
-	Read(archivo, datosMapa.nave.posicionX, datosMapa.nave.posicionY);
-	// Guardar posicion planetaT (X,Y)
-	Read(archivo, datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY);
+  // Guardar fila y columna en el objeto
+  Read(archivo, datosMapa.dimensiones.fil, datosMapa.dimensiones.col);
+  // Guardar posicion nave     (X,Y)
+  Read(archivo, datosMapa.nave.posicionX, datosMapa.nave.posicionY);
+  // Guardar posicion planetaT (X,Y)
+  Read(archivo, datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY);
 
   // Guarda cantidad y coordenadas de estrellas
-	leerCantidadYCoordenadas(archivo, datosMapa.estrellas.cantidad, datosMapa.estrellas.coordenadas);
-	// Guardar cantidad y coordenadas de destructores
-	leerCantidadYCoordenadas(archivo, datosMapa.destructores.cantidad, datosMapa.destructores.coordenadas);
+  leerCantidadYCoordenadas(archivo, datosMapa.estrellas.cantidad, datosMapa.
+                           estrellas.coordenadas);
+  // Guardar cantidad y coordenadas de destructores
+  leerCantidadYCoordenadas(archivo, datosMapa.destructores.cantidad, datosMapa.
+                           destructores.coordenadas);
 
   Close(archivo);
 End;
 
+
+
+
 // Mostrar cantidad y coordenadas Procedure reutilizable para (ESTRELLAS Y DESTRUCTORES)
-procedure MostrarCantidadYCoordenadas(cantidad: Integer; coordenadas: array of coordenada; mensaje: String);
-var
+Procedure MostrarCantidadYCoordenadas(cantidad: Integer; coordenadas: Array Of
+                                      coordenada; mensaje: String);
+
+Var 
   i: Integer;
-begin
+Begin
   writeLn('Cantidad de ', mensaje, ': ', cantidad);
   writeln('Coordenadas de ', mensaje, ':');
-  for i := 1 to cantidad do
-  begin
-    writeln(i, ': X=', coordenadas[i].posicionX, ', Y=', coordenadas[i].posicionY);
-  end;
-end;
+  For i := 1 To cantidad Do
+    Begin
+      writeln(i, ': X=', coordenadas[i].posicionX, ', Y=', coordenadas[i].
+              posicionY);
+    End;
+End;
 
-procedure LlenarMatriz(var m: matriz; datosMapa: dataMapa);
-var
+Procedure LlenarMatriz(Var m: matriz; datosMapa: dataMapa);
+
+Var 
   i,j: Integer;
-begin
+Begin
   // Inicializar la matriz con espacios en blanco
-  for i := 1 to limite do
-    for j := 1 to limite do
+  For i := 1 To limite Do
+    For j := 1 To limite Do
       m[i, j] := '#';
 
-	// Asignar posicion de la nave en la matriz
-	m[datosMapa.nave.posicionX, datosMapa.nave.posicionY] := 'H';
+  // Asignar posicion de la nave en la matriz
+  m[datosMapa.nave.posicionX, datosMapa.nave.posicionY] := 'H';
 
-	// Asignar posicion del planetaT en la matriz
-	m[datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY] := 'T';
+  // Asignar posicion del planetaT en la matriz
+  m[datosMapa.planetaT.posicionX, datosMapa.planetaT.posicionY] := 'T';
 
   // Asignar las coordenadas de las estrellas en la matriz
-  for i := 1 to datosMapa.estrellas.cantidad do
-    m[datosMapa.estrellas.coordenadas[i].posicionX, datosMapa.estrellas.coordenadas[i].posicionY] := 'E';
+  For i := 1 To datosMapa.estrellas.cantidad Do
+    m[datosMapa.estrellas.coordenadas[i].posicionX, datosMapa.estrellas.
+    coordenadas[i].posicionY] := 'E';
 
   // Asignar las coordenadas de los destructores en la matriz
-  for i := 1 to datosMapa.destructores.cantidad do
-    m[datosMapa.destructores.coordenadas[i].posicionX, datosMapa.destructores.coordenadas[i].posicionY] := 'D';
-end;
+  For i := 1 To datosMapa.destructores.cantidad Do
+    m[datosMapa.destructores.coordenadas[i].posicionX, datosMapa.destructores.
+    coordenadas[i].posicionY] := 'D';
+End;
 
 
 Procedure imprimirMatriz(m:matriz; datosMapa: dataMapa);
-Var
-  i,j:Integer;
+
+Var 
+  i,j: Integer;
 Begin
 
-  for i:=1 to datosMapa.dimensiones.fil Do
-	Begin
-	  for j:=1 to datosMapa.dimensiones.col Do
-		Begin
-		  write(m[i,j], ' ');
-		End;
-		writeLn;
-	End;
+  For i:=1 To datosMapa.dimensiones.fil Do
+    Begin
+      For j:=1 To datosMapa.dimensiones.col Do
+        Begin
+          write(m[i,j], ' ');
+        End;
+      writeLn;
+    End;
 End;
 
 
 Begin
 
-  Assign(archivo, 'E:\Default Folders\Samuel\Desktop\UCAB 2do semestre\project-ucab\est.dat');
-	leerArchivo(archivo, datosMapa);
-	writeLn('El valor de filas es ', datosMapa.dimensiones.fil, ' y de columnas ', datosMapa.dimensiones.col);
-	writeLn('Las coordenadas de la nave son: ', datosMapa.nave.posicionX, ' y ', datosMapa.nave.posicionY);
-	writeLn('Las coordenadas de el planeta T son: ', datosMapa.planetaT.posicionX, ' y ', datosMapa.planetaT.posicionY);
-  MostrarCantidadYCoordenadas(datosMapa.estrellas.cantidad, datosMapa.estrellas.coordenadas, 'estrellas');
-  MostrarCantidadYCoordenadas(datosMapa.destructores.cantidad, datosMapa.destructores.coordenadas, 'destructores');
-	LlenarMatriz(m, datosMapa);
-	imprimirMatriz(m, datosMapa);
-Readln;
+  Assign(archivo, 'est.dat');
+  leerArchivo(archivo, datosMapa);
+  writeLn('El valor de filas es ', datosMapa.dimensiones.fil, ' y de columnas ',
+          datosMapa.dimensiones.col);
+  writeLn('Las coordenadas de la nave son: ', datosMapa.nave.posicionX, ' y ',
+          datosMapa.nave.posicionY);
+  writeLn('Las coordenadas de el planeta T son: ', datosMapa.planetaT.posicionX,
+          ' y ', datosMapa.planetaT.posicionY);
+  MostrarCantidadYCoordenadas(datosMapa.estrellas.cantidad, datosMapa.estrellas.
+                              coordenadas, 'estrellas');
+  MostrarCantidadYCoordenadas(datosMapa.destructores.cantidad, datosMapa.
+                              destructores.coordenadas, 'destructores');
+  LlenarMatriz(m, datosMapa);
+  imprimirMatriz(m, datosMapa);
+  Readln;
 End.
